357fead4ed85e93e9d3406bc86b220966616146d
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2025-06-20 15:38:55 -0300

    remove nvim & vscode settings from dotfiles

--- nvim/lua/plugins/colorizer.lua
+++
@@ -1,9 +0,0 @@
--- A high-performance color highlighter for Neovim which has no external dependencies! Written in performant Luajit.
--- https://github.com/norcalli/nvim-colorizer.lua
-return {
-	"catgoose/nvim-colorizer.lua",
-	event = "BufReadPre",
-	config = function()
-		require("colorizer").setup()
-	end,
-}
--- nvim/lua/plugins/gitsigns.lua
+++
@@ -1,5 +0,0 @@
--- Git integration for buffers
--- https://github.com/lewis6991/gitsigns.nvim
-return {
-	"lewis6991/gitsigns.nvim",
-}
